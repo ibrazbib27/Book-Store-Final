9c0061af198114cf23ecca807f79b94840be4bd6
Ibrahim Zbib <abraham.zbib@gmail.com>
2020-10-03 13:04:31 -0400

    navbar and routing for frontend complete

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,27 +1,67 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import BooksNavbar from "./components/books_navbar/BooksNavbar";
 
-const App = (props: AppProps) => {
-	const [greeting, setGreeting] = React.useState<string>('');
 
-	React.useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch('/api/sup');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
+export interface bookType
+{
+	id: number;
+	title: string;
+	author: string;
+	price: number;
+	name: string;
+	categoryid: number;
+}
+interface AppProps {}
+
+const App: React.FC<AppProps> = (props) => {
 
 	return (
-		<div className="min-vh-100 d-flex justify-content-center align-items-center">
-			<h1 className="display-1">Sup {greeting}!</h1>
-		</div>
+		<Router>
+			<Switch>
+				{['/'].map((path) => (
+					<Route
+					exact
+					path={path}
+					key={path}
+					render={(props) => (
+						<>
+							<BooksNavbar  history={props.history} location={props.location} match={props.match} />
+							<Container fluid className={'bg-color w-100 d-flex align-items-center justify-content-center min-vh-100'}>
+								<p className={'display-2'}>Welcome!</p>
+							</Container>
+						</>
+					)}
+					/>
+				))}
+				{['/login', '/register'].map((path) => (
+					<Route
+						exact
+						path={path}
+						key={path}
+						render={(props) => (
+							<>
+								<BooksNavbar  history={props.history} location={props.location} match={props.match} />
+								<Container fluid className={'bg-color w-100 d-flex justify-content-center min-vh-100 p-0'}>
+									<Row className={'w-100 align-items-center justify-content-center p-0 mb-0 mx-0 mt-5'}>
+										<Col xs={11} sm={10} md={9} lg={8} xl={7}>
+
+										</Col>
+
+									</Row>
+									<p className={'display-2'}>Welcome!</p>
+								</Container>
+							</>
+						)}
+					/>
+				))}
+			</Switch>
+		</Router>
 	);
 };
 
-interface AppProps {}
 
 export default App;
---
+++ src/client/components/books_navbar/BooksNavbar.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import {useEffect, useState} from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { matchPath } from "react-router";
+
+
+interface BooksNavbarProps extends RouteComponentProps<any> {}
+
+
+
+const BooksNavbar: React.FC<BooksNavbarProps> = (props) => {
+    const token: any = localStorage.getItem('token');
+
+    const [size, setSize] = useState<number>(991.98);
+    const bigScreenClass: string = 'm-3 rounded border border-white text-center'
+
+    useEffect(() => {
+        const screenSize = () => setSize(window.innerWidth);
+        window.addEventListener('resize', screenSize);
+        screenSize();
+        return () => window.removeEventListener('resize', screenSize);
+    }, []);
+
+    const findActiveLink = (path: string) => {
+        const match: any = matchPath(props.location.pathname, {exact: true, path});
+
+        return match !== null && match.params.id !== 'new';
+    }
+
+    return(
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand as={Link} to={'/'} >Book Store</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+
+                <Nav className={`small text-white ${size >= 991.98 ? 'ml-auto' : 'mt-3 rounded border border-white mx-auto'}`}>
+                    <Nav.Link as={Link} eventKey={1} to={'/'} active={findActiveLink('/')} className={size >= 991.98 ? 'mx-5' : bigScreenClass}>Home</Nav.Link>
+                    <Nav.Link as={Link} eventKey={2} to={'/books'} active={findActiveLink('/books') || findActiveLink('/books/:id') || findActiveLink('/books/:id/update')} className={size >= 991.98 ? 'mx-5' : bigScreenClass}>View Books</Nav.Link>
+                    <Nav.Link as={Link} eventKey={3} to={token ? '/books/new' : '/login'} active={findActiveLink(token ? '/books/new' : '/login')} className={size >= 991.98 ? 'mx-5' : bigScreenClass}>{token ? 'Add Book' : 'Login'}</Nav.Link>
+                    <Nav.Link as={Link} eventKey={4} to={token ? props.location.pathname : '/register'} active={findActiveLink('/register')} className={size >= 991.98 ? 'ml-5' : bigScreenClass}
+                              onClick={(e: React.MouseEvent) => {
+                                  if(token){
+                                      localStorage.clear();
+                                      location.replace(location.origin + '/');
+                                  }
+                              }}
+                    >{token ? 'Logout' : 'Register'}</Nav.Link>
+
+
+
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+
+
+};
+
+export default BooksNavbar;
